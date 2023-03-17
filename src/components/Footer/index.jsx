8a1b69c2fe7981da607fb8a2d304fd98b0853536
qualityseg <128048171+qualityseg@users.noreply.github.com>
2023-03-17 10:39:51 -0300

    353

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -12,7 +12,7 @@ export const Footer = ({ children, link = "/" }) => {
               <Image
                 src="/icons/logo_transparente.png"
                 alt="Logo"
-                width="300"
+                width="353"
                 height="110"
               />
             </a>
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -40,7 +40,7 @@ export const Header = () => {
               <Image
                 src="/icons/logo_transparente.png"
                 alt="Eletro Santa Clara"
-                width="300"
+                width="353"
                 height="110"
               />
             </a>
