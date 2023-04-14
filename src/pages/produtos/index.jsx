0c203c9bdbe5235c0c1b552f74baaa68cb24b965
qualityseg <128048171+qualityseg@users.noreply.github.com>
2023-04-14 17:02:15 -0300

    sensei

--- src/pages/produtos/index.jsx
+++ src/pages/produtos/index.jsx
@@ -48,7 +48,7 @@ export default function Tratamentos() {
           <div className={styles.description}>
             <p>
             
-            <Link href="https://www.instagram.com/qualitysegconsultoria/">
+            <Link href="https://app.powerbi.com/view?r=eyJrIjoiMmYzMjhkZDktN2ZjNi00ZDkxLWIwNzctMGFkYjI4MzkyYWUzIiwidCI6IjBhZDFmNWJiLWU3NGItNDdiYi04YmU5LTBkNGUxYzNlZjljZiJ9">
                   <a target="_blank">
                   <p>Confira o BI   </p>
                     <Image
